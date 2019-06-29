057593c9e9f6dc73b1e10390078d40b9c4520ad4
Katerina Greif <katerina@greif.cz>
2019-06-29 22:34:08 +0200

    added docstring

--- src/calc_location.py
+++ src/calc_location.py
@@ -3,6 +3,14 @@ from src.locations import dict_location
 
 
 def get_distance(input_x: int, input_y: int, input_z: int, location: dict):
+    """
+    Calculates distance based on inputs.
+    :param input_x:
+    :param input_y:
+    :param input_z:
+    :param location:
+    :return:
+    """
     a = math.sqrt(
         math.pow(input_x - location['x'], 2) + math.pow(input_y - location['y'], 2) + math.pow(input_z - location['z'],
                                                                                                2)
